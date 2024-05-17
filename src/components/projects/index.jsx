f244229a2b5c065f1fbbc321b838648b5a00fae8
elkinjuniorpa09 <elkinjuniorpa09@gmail.com>
2024-05-17 10:34:56 -0500

    add photos

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -29,19 +29,20 @@ const Projects = () => {
     {
       name: 'Tekneo',
       description: "Se trata de una solución tecnológica destinada a mejorar la eficacia y la seguridad en las empresas, permitiendo visualizar las cámaras de seguridad, llevar un registro de las horas de entrada y gestionar el acceso a las distintas zonas conectadas a los sistemas domóticos.",
+      images: ['/img/tekneo1.jpg', '/img/tekneo2.jpg','/img/tekneo3.jpg','/img/tekneo4.jpg'],
     },
     {
       name: 'Node maker',
       description: "Es un proyecto que simplifica la creación y gestión de fuentes de datos KNX y MQTT en una red local. Facilita la comunicación entre ellas, permite crear perfiles y rutas, y ofrece una interfaz de supervisión para la visualización de datos. El software agiliza la transferencia de datos entre fuentes, promoviendo una gestión eficiente de la red.",
+      images: ['/img/node1.jpg', '/img/node2.jpg'],
     },
     {
       name: 'Mvsion',
       description: "MVision es un software para gestionar fotos y videos en pantallas de salas de espera y publicidad. Permite crear y configurar pantallas, subir contenido, y definir horarios y orden de reproducción. Las pantallas se conectan a un hardware controlado por MVision para administrar el modo quiosco.",
+      
       images: ['/img/login.jpg', '/img/img1.jpg','/img/img2.jpg','/img/img3.jpg','/img/img4.jpg'],
     },
   ];
-
-
 
   return (
     <div className="p-10 bg-slate-950 overflow-y-auto">
@@ -51,13 +52,13 @@ const Projects = () => {
           <motion.div key={index} whileHover={{ scale: 1.05 }} className="bg-slate-800/90 p-4 shadow-2xl rounded-2xl">
             <div className='flex items-center mb-2 justify-between'>
               <h2 className="text-xl text-green-400 font-semibold mb-2 inline-flex items-end">{project.name}</h2>
-              {project.name === 'Mvsion' ? ( 
-                    <button 
-                    onClick={() => openModal(project)} 
-                    className="flex items-center gap-3 justify-center px-1 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-                  >
+              {project.images ? (
+                <button 
+                  onClick={() => openModal(project)} 
+                  className="flex items-center gap-3 justify-center px-1 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                >
                   Ver <BsFillImageFill className="mr-2" />
-                  </button>
+                </button>
               ) : (
                 <IconJamPadlockF className="w-6" /> 
               )}
